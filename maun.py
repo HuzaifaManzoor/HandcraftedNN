66c5c78e3ab99d515df93ba980897afed5764bc9
HuzaifaManzoor <huzaifa4051@gmail.com>
2024-02-26 13:22:44 +0500

    add dense layes

--- maun.py
+++ maun.py
@@ -30,13 +30,60 @@
 
 
 ################### Now we are using vectors and metrics  dont raw python #################
-input = [1,2,3, 2.5]
+# import numpy as np 
+# input = [1,2,3, 2.5]
 
 
-weights =  [[0.2 , 0.8 , -0.5,1-0],[0.5, -0.91, 0.26 ,- 0.5],[-0.26, -0.27 , 0.17 , 0.87] ]
-bias = [2, 3, 0.5]
+# weights =  [[0.2 , 0.8 , -0.5,1-0],[0.5, -0.91, 0.26 ,- 0.5],[-0.26, -0.27 , 0.17 , 0.87] ]
+# weights_array = np.array(weights)
+# input_array = np.array(input)
+# bias = [2, 3, 0.5]
 
-import numpy as np
+# import numpy as np
 
-output = np.dot(weights, input) + bias
-print(output)
+# # output = np.dot(weights, input_array.T) + bias
+# # print(output)
+# print(weights_array.T)
+# print(input_array.shape)
+
+
+#######################
+
+# batch of input values and modeling a 2 layers of neurons 
+# import numpy as np
+# input = [[1,2,3, 2.5],
+#          [2.0,5.0,-1.0,2.0],
+#          [-1.5,2.7,3.3,-0.8]]
+# input_array = np.array(input)
+# weights = [[0.2 , 0.8 , -0.5,1-0],
+#            [0.5, -0.91, 0.26 ,- 0.5],
+#            [-0.26, -0.27 , 0.17 , 0.87] ]
+
+# bias = [2, 3, 0.5]
+
+# weights2 = [[0.1 , -0.14 , 0.5],
+#            [-0.5, 0.12,-0.33 ],
+#            [-0.44, 0.73 , -0.13] ]
+
+# bias2 = [-1 , 2 , -0.5]
+
+# layer1 = np.dot(input , np.array(weights).T) + bias
+# layer2 = np.dot(layer1 , np.array(weights2).T) + bias2
+
+# print(layer2)
+
+
+##  now we gonna do the things in objects 
+
+X = [[1,2,3, 2.5],
+     [2.0,5.0,-1.0,2.0],
+     [-1.5,2.7,3.3,-0.8]]
+
+
+
+
+class Layer_Dense:
+    def __init__(self):
+        pass
+    def forward(self):
+        pass
